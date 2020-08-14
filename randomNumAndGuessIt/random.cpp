406f105cd61d53a1ce4a1842411dfa4926503e46
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-14 17:47:24 +0700

    edit line 12

--- randomNumAndGuessIt/random.cpp
+++ randomNumAndGuessIt/random.cpp
@@ -9,7 +9,7 @@ int main()
     int a[5];
     int inputnum;
     printf("input your max range of number that you want to random  : ");
-        scanf("%d", &inputnum);
+    scanf("%d", &inputnum);
     for (int i = 0; i < 3; i++)
     {
         scanf("%d", &a[i]);
